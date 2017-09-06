dfc90444af610232cbe74f0fb4d1b7a8a2c8f905
System <none>
2017-09-06 20:01:58 +0000

    💈😬 Updated with Glitch

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
+var offset=1;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/api/imagesearch/:query", function (req, res) {
+  var url = "https://www.googleapis.com/customsearch/v1?q=" + req.params.query 
+    + "&cx=" + process.env.Engine_ID
+    + "&num=10&start=" + Number(1 + parseInt(offset)*10)
+    + "&fileType=bmp,png,gif,jpg,jpeg"
+    + "&key=" + process.env.Google_API_key;
+  res.send(url);
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+app.get("/api/latest/imagesearch/", function (request, response) {
+  
 });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
