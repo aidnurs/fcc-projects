b9a36dfe865a57a33cb97ad098e927d8f55b681f
System <none>
2017-09-08 21:15:17 +0000

    search and latest

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var app = express();
+var mongo= require('mongodb').MongoClient;
+var mongoUri="mongodb://"+process.env.user+":"+process.env.password+"@ds131384.mlab.com:31384/imgsearch"
 
 const GoogleImages = require('google-images');
  
@@ -12,10 +14,30 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/cats',function(req,res){
+app.get('/api/imagesearch/:input',function(req,res){
+  var input=req.params.input;
+  mongo.connect(mongoUri,function(err,db){
+    db.collection('imgsearch').insert({
+      term:input,
+      when:new Date()
+    });
+    db.close();
+  });
+  
   var obj={};
   client.search('cats').then(images => {
     res.json(images);
+  });
+});
+
+app.get('/api/latest/imagesearch/',function(req,res){
+  mongo.connect(mongoUri,function(err,db){
+    db.collection('imgsearch').find({},{_id: 0, term: 1, when: 1}).toArray()
+      .then(results => {
+        db.close();
+        res.json(results);
+      })
+    db.close();
   });
 });
 
